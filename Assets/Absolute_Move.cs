36ab16acdf906826e44128f4c8d6ff49d1808453
aventure19 <hideride@outlook.jp>
2016-08-08 16:45:54 +0900

    from aventure19

---
+++ Assets/Absolute_Move.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Absolute_Move : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 v = this.transform.position;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            v.x -= 0.05f;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            v.x += 0.05f;
+        }
+
+        this.transform.position = v;
+    }
+}
