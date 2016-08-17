af0b1b6ee34c8f8748d72fb0bfd3df3e943f41ec
aventure19 <hideride@outlook.jp>
2016-08-17 12:18:04 +0900

    aventure19 AnimeTest
    アニメーション遷移スクリプト&テストシーンの追加

---
+++ Assets/Scripts/IsWalk_Bool.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IsWalk_Bool : MonoBehaviour
+{
+
+    public string up, right, left;
+
+    private Animator animator;
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(up) == true)
+        {
+            transform.position += transform.forward * 0.01f;
+            animator.SetBool("IsWalk_Bool", true);
+        }
+        else {
+            animator.SetBool("IsWalk_Bool", false);
+        }
+        if (Input.GetKey(right) == true)
+        {
+            transform.Rotate(0, 10, 0);
+        }
+        if (Input.GetKey(left))
+        {
+            transform.Rotate(0, -10, 0);
+        }
+    }
+}
