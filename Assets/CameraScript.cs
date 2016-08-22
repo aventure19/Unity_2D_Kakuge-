946e216144a5d30b7280c37b974d0a3f51d5184a
Taka8 <thnx4joiningus@gmail.com>
2016-08-22 18:26:14 +0900

    新しくシーンを追加＆プロト版

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public Transform target1;
+    public Transform target2;
+    public Vector3 CameraOffset = new Vector3(0, 2.5f, -5f);
+
+    private float StartDistance;
+
+    void Start()
+    {
+
+        StartDistance = Vector3.Distance(target1.position, target2.position);
+
+    }
+
+    void OnPreRender()
+    {
+
+        float CurrentDistance = Vector3.Distance(target1.position, target2.position);
+
+        float t = CurrentDistance / StartDistance;
+
+        Camera.main.transform.position = (target1.position + target2.position) / 2 + CameraOffset * t;
+
+    }
+
+}
---
+++ Assets/ElementScript.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(CharacterController))]
+public class ElementScript : MonoBehaviour
+{
+
+    public float Speed = 5;
+    public float JumpPower = 10;
+    public float Gravity = 9.8f;
+
+    private Animator animator;
+    private CharacterController controller;
+
+    [SerializeField]
+    private Vector3 moveDirection;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // 地上
+        if (controller.isGrounded == true)
+        {
+
+            // 水平移動
+            moveDirection.x = Input.GetAxis("Horizontal");
+
+            // ジャンプ
+            if (Input.GetAxis("Vertical") > 0.8f)
+            {
+
+                moveDirection.y = JumpPower;
+
+                animator.SetTrigger("Jump");
+
+            }
+
+            animator.SetBool("IsGrounded", true);
+
+            // パンチ
+            if (Input.GetButtonDown("Fire1") == true) animator.SetTrigger("Punch");
+
+            // キック
+            if (Input.GetButtonDown("Fire2") == true) animator.SetTrigger("Kick");
+
+        }
+        // 空中
+        else
+        {
+
+            animator.SetBool("IsGrounded", false);
+
+            moveDirection.y -= Gravity * Time.deltaTime;
+
+        }
+
+        // 移動確定
+        controller.Move(moveDirection * Speed * Time.deltaTime);
+
+        // アニメーターに横移動速度を渡す
+        animator.SetFloat("Speed", Mathf.Abs(moveDirection.x));
+
+    }
+}
